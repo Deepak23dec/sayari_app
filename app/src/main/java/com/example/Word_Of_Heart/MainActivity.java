4f581c182ef742de7593f0a83a8d08674aec619d
unknown <singhdeepak4521@gmail.com>
2023-10-06 02:28:23 +0530

    deepak

--- app/src/main/java/com/example/Word_Of_Heart/MainActivity.java
+++ app/src/main/java/com/example/Word_Of_Heart/MainActivity.java
@@ -6,8 +6,6 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.majnuleleshayari.R;
-
 public class MainActivity extends AppCompatActivity {
     //project done by Deepak Chaudhary and Ananya Varshney
     @Override
--- app/src/main/java/com/example/Word_Of_Heart/startexit.java
+++ app/src/main/java/com/example/Word_Of_Heart/startexit.java
@@ -6,8 +6,6 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.majnuleleshayari.R;
-
 public class startexit extends AppCompatActivity {
 
     //project done by Deepak Chaudhary and Ananya Varshney
